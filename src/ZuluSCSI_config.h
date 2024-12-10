b6ff59401957c4cdabae1d509c50124fd255a487
Alex Perez <aperez@rabbithole.ws>
2024-12-09 18:15:46 -0800

    Prepare for v2024.12.09 v1.1plus hotfix release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -29,7 +29,7 @@
 
 // Use variables for version number
 #define FW_VER_NUM      "24.12.09"
-#define FW_VER_SUFFIX   "dev"
+#define FW_VER_SUFFIX   "v1.1plus-hotfix-release"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
