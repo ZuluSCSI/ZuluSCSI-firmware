dbfe0761033e32aee1f63a402959f7b9e10b8743
Alex Perez <aperez@rabbithole.ws>
2025-08-26 12:14:09 -0700

    Prepare for v2025.08.26 release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -28,7 +28,7 @@
 
 // Use variables for version number
 #define FW_VER_NUM      "25.08.26"
-#define FW_VER_SUFFIX   "dev"
+#define FW_VER_SUFFIX   "release"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
