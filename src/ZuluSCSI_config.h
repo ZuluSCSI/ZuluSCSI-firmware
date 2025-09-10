16ed6bf658456f70fcea482a7f5ceca361bdb06d
Alex Perez <aperez@rabbithole.ws>
2025-09-10 16:46:34 -0700

    Prepare for v2025.09.10 release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,7 +27,7 @@
 #include <ZuluSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "25.09.04"
+#define FW_VER_NUM      "25.09.10"
 #define FW_VER_SUFFIX   "release"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
