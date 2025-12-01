ZuluSCSI/ZuluSCSI-firmware

commit 695d7b2f0c21052159adcc9026f9e833f2889c37

Fix OLED error message when no i2c bridge present, and display initiator status on OLED (#707)
* Display initiator status on OLED without controls

If there is no I2C GPIO expander for navigation controls, still
display the initiator OLED screens.

Also fixed the displaying an error message in device mode when a display
is detected but no GPIO expander is found. Previously it just displayed
a line without text.

* Use splash screen and display create file progress

This returns the splash screen when no controller board is found but
a display exist. It also displays the version number on the error
screen. The error screen has the SD icon and it will indicate if
the SD card is present or not.

Because of the code path, the screen saver will trigger too.

Enabled all the progress bar displays too. Like the CREATE file,
ROM image copying, and kiosk modes. It returns to the error screen
after the progress bar finishes.

lib/ZuluSCSI_UI_RP2MCU/control.cpp
@@ -241,7 +241,12 @@ static bool splashScreenPoll()
         {
             g_uiStart = ZULUSCSI_UI_START_DONE;
 
-            if (!g_sdcard_present)
+            if (!g_controlBoardEnabled)
+            {
+                changeScreen(SCREEN_NO_CONTROLS_ERROR, SCREEN_ID_NO_PREVIOUS);
+                g_activeScreen->tick();
+            }
+            else if (!g_sdcard_present)
             {
                 switch(g_systemMode)
                 {
@@ -480,27 +485,8 @@ bool initControlBoardHardware()
 
     g_reverseRotary = !cfg->controlBoardReverseRotary;
 
-    if (!initControlBoardI2C())
-    {
-        logmsg("Failed to init Control Board Input I2C chip. Disabling Control Board functionality");
-
-        // Disable Control board as it couldn't be initalized
-        g_controlBoardEnabled = false;
-
-        g_display->clearDisplay();
-        g_display->setCursor(0,0);             
-        g_display->print(F("ZuluSCSI Control"));
-        g_display->drawLine(0,10,127,10, 1);
-        g_display->setCursor(0,16);             
-        g_display->print(F("Failed to Init I2C chip"));
-        g_display->display(); 
-
-        return false;
-    }
-
     enableScreen(true);
-
-    g_controlBoardEnabled = true;
+    g_controlBoardEnabled = initControlBoardI2C();
     return true;
 }
 
@@ -685,7 +671,7 @@ void initUIPostSDInit(bool sd_card_present)
         g_uiSplashStartTime = millis();
     }
 
-    logmsg("Control Board is ", g_controlBoardEnabled?"Enabled.":"Disabled.");
+    logmsg("Control Board is ", g_controlBoardEnabled ? "Enabled" : "Disabled");
 }
 
 void updateRotary(int dir)
@@ -763,11 +749,9 @@ void scanForNestedFolders()
 }
 
 /////// THIS IS WHERE WE CAN INIT, THE DEVICE LIST IS VALID HERE
-// Called on sd remove and Device Chnages
+// Called on sd remove and Device Changes
 void stateChange()
 {
-    // printDevices();
-
     g_userInputDetected = true;  // Something changed, so assume the user did interact
 
     sendSDCardStateChangedToScreens(g_sdAvailable);
@@ -797,7 +781,10 @@ void stateChange()
     switch(g_systemMode)
     {
         case SYSTEM_NORMAL:
-            changeScreen(SCREEN_MAIN, SCREEN_ID_NO_PREVIOUS);
+            if (g_controlBoardEnabled)
+                changeScreen(SCREEN_MAIN, SCREEN_ID_NO_PREVIOUS);
+            else
+                changeScreen(SCREEN_NO_CONTROLS_ERROR, SCREEN_ID_NO_PREVIOUS);
             g_activeScreen->tick();
             break;
             
@@ -872,11 +859,6 @@ void processRotaryEncoder(uint8_t input_byte)
 // When this is called, the device list is complete, either on startup or on a card swap
 void devicesUpdated()
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     stateChange();
 }
 
@@ -1200,20 +1182,16 @@ extern "C" void binCueInUse(int target_idx, const char *foldername)
 // When a card is removed or inserted. If it's removed then clear the device list
 extern "C" void sdCardStateChanged(bool sdAvailable)
 {
+
     g_sdAvailable = sdAvailable;
 
-    initDevices();
+    if (!hasControlBeenInitialized) return;
 
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
+    initDevices();
 
     if (!sdAvailable) // blank the device map
     {
         patchDevices();
-        // printDevices();
-
         stateChange();
     }
 }
@@ -1221,11 +1199,6 @@ extern "C" void sdCardStateChanged(bool sdAvailable)
 // On startup on when a card has finished been reinserted, this is call. Used to call 2nd pass of device setup
 extern "C" void scsiReinitComplete()
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     patchDevices();
     devicesUpdated();
 }
@@ -1262,95 +1235,91 @@ extern "C" bool mscMode()
 
 extern "C" void controlLoop()
 {
-    if (!g_controlBoardEnabled || splashScreenPoll())
+    if (splashScreenPoll())
     {
         return;
     }
-
     pollUI();
 
-    // Get the input from the control board
-    uint8_t input_byte = getValue();
-
-    processRotaryEncoder(input_byte);
-    processButtons(input_byte);
-
-    if (g_activeScreen != NULL)
+    if (g_controlBoardEnabled)
     {
-        if (g_shortPressed[2])
-        {
-            if (!g_screenSaverActive)
-            {
-                g_activeScreen->shortRotaryPress();
-            }
-            g_userInputDetected = true;
-        }
-        if (g_longPressed[2])
-        {
-            if (!g_screenSaverActive)
-            {
-                g_activeScreen->longRotaryPress();
-            }
-            g_userInputDetected = true;
-        }
-        if (g_shortPressed[0])
-        {
-            if (!g_screenSaverActive)
-            {
-                g_activeScreen->shortUserPress();
-            }
-            g_userInputDetected = true;
-        }
-        if (g_longPressed[0])
-        {
-            if (!g_screenSaverActive)
-            {   
-                g_activeScreen->longUserPress();
-            }
-            g_userInputDetected = true;
-        }
-        if (g_shortPressed[1])
-        {
-            if (!g_screenSaverActive)
-            {
-                g_activeScreen->shortEjectPress();
-            }
-            g_userInputDetected = true;
-        }
-        if (g_longPressed[1])
-        {
-            if (!g_screenSaverActive)
-            {
-                g_activeScreen->longEjectPress();
-            }
-            g_userInputDetected = true;
-        }
+        // Get the input from the control board
+        uint8_t input_byte = getValue();
 
-        screenSaverLoop();
+        processRotaryEncoder(input_byte);
+        processButtons(input_byte);
 
-        if (!g_screenSaverActive)
+        if (g_activeScreen != NULL)
         {
-            if (g_activeScreen != NULL)
+            if (g_shortPressed[2])
             {
-                g_activeScreen->tick();
+                if (!g_screenSaverActive)
+                {
+                    g_activeScreen->shortRotaryPress();
+                }
+                g_userInputDetected = true;
             }
-        }
-        else
-        {
-            drawScreenSaver();
+            if (g_longPressed[2])
+            {
+                if (!g_screenSaverActive)
+                {
+                    g_activeScreen->longRotaryPress();
+                }
+                g_userInputDetected = true;
+            }
+            if (g_shortPressed[0])
+            {
+                if (!g_screenSaverActive)
+                {
+                    g_activeScreen->shortUserPress();
+                }
+                g_userInputDetected = true;
+            }
+            if (g_longPressed[0])
+            {
+                if (!g_screenSaverActive)
+                {   
+                    g_activeScreen->longUserPress();
+                }
+                g_userInputDetected = true;
+            }
+            if (g_shortPressed[1])
+            {
+                if (!g_screenSaverActive)
+                {
+                    g_activeScreen->shortEjectPress();
+                }
+                g_userInputDetected = true;
+            }
+            if (g_longPressed[1])
+            {
+                if (!g_screenSaverActive)
+                {
+                    g_activeScreen->longEjectPress();
+                }
+                g_userInputDetected = true;
+            }
         }
     }
+    screenSaverLoop();
 
+    if (!g_screenSaverActive)
+    {
+        if (g_activeScreen != NULL)
+        {
+            g_activeScreen->tick();
+        }
+    }
+    else
+    {
+        drawScreenSaver();
+    }
     g_userInputDetected = false;
 }
 
 // Create 
 void UICreateInit(uint64_t blockCount, uint32_t blockSize, const char *filename) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
 
     _copyScreen->TotalRetries = 0;
     _copyScreen->TotalErrors  = 0;
@@ -1368,10 +1337,6 @@ void UICreateInit(uint64_t blockCount, uint32_t blockSize, const char *filename)
 
 void UICreateProgress(uint32_t blockTime, uint32_t blockCopied) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
     _copyScreen->BlockTime = blockTime;
     _copyScreen->BlocksCopied = blockCopied;
     _copyScreen->BlocksInBatch = 1;
@@ -1382,10 +1347,6 @@ void UICreateProgress(uint32_t blockTime, uint32_t blockCopied)
 /// Kiosk
 void UIKioskCopyInit(uint8_t deviceIndex, uint8_t totalDevices, uint64_t blockCount, uint32_t blockSize, const char *filename)
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
     _copyScreen->TotalRetries = 0;
     _copyScreen->TotalErrors  = 0;
 
@@ -1416,10 +1377,6 @@ void UIKioskCopyInit(uint8_t deviceIndex, uint8_t totalDevices, uint64_t blockCo
 
 void UIKioskCopyProgress(uint32_t blockTime, uint32_t blockCopied)
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
     _copyScreen->BlockTime = blockTime;
     _copyScreen->BlocksCopied = blockCopied;
     _copyScreen->BlocksInBatch = 1;
@@ -1431,10 +1388,6 @@ void UIKioskCopyProgress(uint32_t blockTime, uint32_t blockCopied)
 /// Rom copy
 void UIRomCopyInit(uint8_t deviceId, S2S_CFG_TYPE deviceType, uint64_t blockCount, uint32_t blockSize, const char *filename)
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
     _copyScreen->TotalRetries = 0;
     _copyScreen->TotalErrors  = 0;
 
@@ -1450,10 +1403,6 @@ void UIRomCopyInit(uint8_t deviceId, S2S_CFG_TYPE deviceType, uint64_t blockCoun
 }
 void UIRomCopyProgress(uint8_t deviceId, uint32_t blockTime, uint32_t blocksCopied) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
     _copyScreen->BlockTime = blockTime;
     _copyScreen->BlocksCopied = blocksCopied;
     _copyScreen->BlocksInBatch = 1;
@@ -1468,11 +1417,6 @@ bool g_initiatorMessageToProcess;
 
 void UIInitiatorScanning(uint8_t deviceId, uint8_t initiatorId)
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     startInitiator(initiatorId);
 
     g_initiatorMessageToProcess = true;
@@ -1502,11 +1446,6 @@ void UIInitiatorScanning(uint8_t deviceId, uint8_t initiatorId)
 
 void UIInitiatorReadCapOk(uint8_t deviceId, S2S_CFG_TYPE deviceType, uint64_t sectorCount, uint32_t sectorSize) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     g_initiatorMessageToProcess = true;
 
     DeviceMap *deviceMap = &g_devices[deviceId];
@@ -1533,10 +1472,6 @@ void UIInitiatorReadCapOk(uint8_t deviceId, S2S_CFG_TYPE deviceType, uint64_t se
 
 void UIInitiatorProgress(uint8_t deviceId, uint32_t blockTime, uint32_t sectorsCopied, uint32_t sectorInBatch) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
     _copyScreen->BlockTime = blockTime;
     _copyScreen->BlocksCopied = sectorsCopied;
     _copyScreen->BlocksInBatch = sectorInBatch;
@@ -1547,11 +1482,6 @@ void UIInitiatorProgress(uint8_t deviceId, uint32_t blockTime, uint32_t sectorsC
 
 void UIInitiatorRetry(uint8_t deviceId) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     g_initiatorMessageToProcess = true;
 
     DeviceMap *deviceMap = &g_devices[deviceId];
@@ -1562,11 +1492,6 @@ void UIInitiatorRetry(uint8_t deviceId)
 
 void UIInitiatorSkippedSector(uint8_t deviceId) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     g_initiatorMessageToProcess = true;
 
     DeviceMap *deviceMap = &g_devices[deviceId];
@@ -1577,11 +1502,6 @@ void UIInitiatorSkippedSector(uint8_t deviceId)
 
 void UIInitiatorTargetFilename(uint8_t deviceId, char *filename) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     g_initiatorMessageToProcess = true;
 
     DeviceMap *deviceMap = &g_devices[deviceId];
@@ -1590,21 +1510,11 @@ void UIInitiatorTargetFilename(uint8_t deviceId, char *filename)
 
 void UIInitiatorFailedToTransfer(uint8_t deviceId) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     g_initiatorMessageToProcess = true;
 }
 
 void UIInitiatorImagingComplete(uint8_t deviceId) 
 {
-    if (!g_controlBoardEnabled)
-    {
-        return;
-    }
-
     g_initiatorMessageToProcess = true;
 
     DeviceMap *deviceMap = &g_devices[deviceId];

src/ZuluSCSI.cpp
@@ -1136,14 +1136,13 @@ static void zuluscsi_setup_sd_card(bool wait_for_card = true)
     do
     {
       blinkStatus(BLINK_ERROR_NO_SD_CARD);
-      if (g_controlBoardEnabled)
+
+      uint32_t input_wait_start = millis();
+      while ((uint32_t)(millis() - input_wait_start) < 500)
       {
-        uint32_t input_wait_start = millis();
-        while ((uint32_t)(millis() - input_wait_start) < 500)
-        {
-          controlLoop();
-        }
+        controlLoop();
       }
+
       platform_reset_watchdog();
       g_sdcard_present = mountSDCard();
 
@@ -1338,12 +1337,10 @@ extern "C" void zuluscsi_main_loop(void)
     diskEjectButtonUpdate(true);
   blink_poll();
 
-  if (g_controlBoardEnabled)
+
+  if (scsiDev.phase == BUS_FREE)
   {
-    if (scsiDev.phase == BUS_FREE)
-    {
-      controlLoop();
-    }
+    controlLoop();
   }
 
 #ifdef ZULUSCSI_NETWORK
@@ -1427,18 +1424,14 @@ extern "C" void zuluscsi_main_loop(void)
         blinkStatus(BLINK_ERROR_NO_SD_CARD);
         platform_reset_watchdog();
         platform_poll();
-        if (g_controlBoardEnabled)
+        uint32_t input_wait_start = millis();
+        while ((uint32_t)(millis() - input_wait_start) < 500)
         {
-          uint32_t input_wait_start = millis();
-          while ((uint32_t)(millis() - input_wait_start) < 500)
+          if (scsiDev.phase == BUS_FREE)
           {
-            if (scsiDev.phase == BUS_FREE)
-            {
-              controlLoop();
-            }
+            controlLoop();
           }
         }
-
       }
     } while (!g_sdcard_present && !g_romdrive_active && !is_initiator && !g_rebooting);
   }

lib/ZuluSCSI_UI_RP2MCU/NoControlsErrorScreen.cpp
@@ -21,28 +21,46 @@
 
 #if defined(CONTROL_BOARD)
 
-#ifndef SCREENTYPE_H
-#define SCREENTYPE_H
+#include "ui.h"
+#include "NoControlsErrorScreen.h"
+#include "SplashScreen.h"
+#include "ZuluSCSI_log.h"
 
-typedef enum
+#include "cache.h"
+
+extern bool g_sdAvailable;
+
+void NoControlsErrorScreen::init(int index)
 {
-    SCREEN_NONE,
-	SCREEN_SPLASH,
-    SCREEN_SETTINGS,
-    SCREEN_MAIN,
-    SCREEN_INFO,
-    SCREEN_INFO_PAGE2,
-    SCREEN_INFO_PAGE3,
-    SCREEN_INFO_PAGE4,
-    SCREEN_BROWSE_TYPE,
-    SCREEN_BROWSE,
-    SCREEN_BROWSE_FLAT,
-    MESSAGE_BOX,
-    SCREEN_COPY,
-    SCREEN_INITIATOR_MAIN,
-    SCREEN_NO_CONTROLS_ERROR
-} SCREEN_TYPE;
+  Screen::init(index);
+}
 
-#endif
+void NoControlsErrorScreen::draw()
+{
+  _display->setCursor(0,0);
+  _display->print(F("ZuluSCSI Error"));
+  _display->drawLine(0,10,112,10, 1);
+  if (g_sdAvailable)
+  {
+    _display->drawBitmap(115, 0, icon_sd, 12,12, WHITE);
+  }
+  else
+  {
+    _display->drawBitmap(115, 0, icon_nosd, 12,12, WHITE);
+  }
+  _display->setCursor(0,16);
+  _display->print(F("Failed to Init I2C"));
+  _display->setCursor(0,28);
+  _display->print(F("GPIO expander for"));
+  _display->setCursor(0,38);
+  _display->print(F("navigation controls"));
+
+  printCenteredText(g_log_short_firmwareversion, 50);
+}
+
+void NoControlsErrorScreen::tick()
+{
+  Screen::tick();
+}
 
 #endif

lib/ZuluSCSI_UI_RP2MCU/NoControlsErrorScreen.h
@@ -19,30 +19,26 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 **/
 
+#pragma once
 #if defined(CONTROL_BOARD)
 
-#ifndef SCREENTYPE_H
-#define SCREENTYPE_H
+#include "Screen.h"
 
-typedef enum
+class NoControlsErrorScreen : public Screen
 {
-    SCREEN_NONE,
-	SCREEN_SPLASH,
-    SCREEN_SETTINGS,
-    SCREEN_MAIN,
-    SCREEN_INFO,
-    SCREEN_INFO_PAGE2,
-    SCREEN_INFO_PAGE3,
-    SCREEN_INFO_PAGE4,
-    SCREEN_BROWSE_TYPE,
-    SCREEN_BROWSE,
-    SCREEN_BROWSE_FLAT,
-    MESSAGE_BOX,
-    SCREEN_COPY,
-    SCREEN_INITIATOR_MAIN,
-    SCREEN_NO_CONTROLS_ERROR
-} SCREEN_TYPE;
+public:
+    NoControlsErrorScreen(Adafruit_SSD1306 *display) : Screen(display), _last_sd_state(false)
+    {
+    }
 
-#endif
+    SCREEN_TYPE screenType() { return SCREEN_NO_CONTROLS_ERROR; }
+
+    void init(int index);
+    void draw();
+    void tick();
+
+private:
+    bool _last_sd_state;
+};
 
 #endif

lib/ZuluSCSI_UI_RP2MCU/UIContainer.cpp
@@ -38,6 +38,7 @@
 #include "MessageBox.h"
 #include "CopyScreen.h"
 #include "InitiatorMainScreen.h"
+#include "NoControlsErrorScreen.h"
 #include "control.h"
 #include <ZuluSCSI_buffer_control.h>
 
@@ -58,6 +59,7 @@ BrowseScreenFlat *_browseScreenFlat;
 MessageBox *_messageBox;
 CopyScreen *_copyScreen;
 InitiatorMainScreen *_initiatorMainScreen;
+NoControlsErrorScreen *_noControlsErrorScreen;
 
 #if ZULUSCSI_RESERVED_SRAM_LEN == 0
 extern Adafruit_SSD1306 allocated_g_display;
@@ -75,6 +77,7 @@ static BrowseScreenFlat static_browseScreenFlat(&allocated_g_display);
 static MessageBox static_messageBox(&allocated_g_display);
 static CopyScreen static_copyScreen(&allocated_g_display);
 static InitiatorMainScreen static_initiatorMainScreen(&allocated_g_display);
+static NoControlsErrorScreen static_noControlsErrorScreen(&allocated_g_display);
 
 void initScreens()
 {
@@ -91,6 +94,7 @@ void initScreens()
     _messageBox = &static_messageBox;
     _copyScreen = &static_copyScreen;
     _initiatorMainScreen = &static_initiatorMainScreen;
+    _noControlsErrorScreen = &static_noControlsErrorScreen;
 }
 
 #else
@@ -111,6 +115,7 @@ void initScreens()
     _messageBox = new (reserve_buffer_align(sizeof(MessageBox), 4)) MessageBox(g_display);
     _copyScreen = new (reserve_buffer_align(sizeof(CopyScreen), 4)) CopyScreen(g_display);
     _initiatorMainScreen = new (reserve_buffer_align(sizeof(InitiatorMainScreen), 4)) InitiatorMainScreen(g_display);
+    _noControlsErrorScreen = new (reserve_buffer_align(sizeof(NoControlsErrorScreen), 4)) NoControlsErrorScreen(g_display);
 }
 #endif
 // Call new card method on all screens
@@ -129,6 +134,7 @@ void sendSDCardStateChangedToScreens(bool cardIsPresent)
     _messageBox->sdCardStateChange(cardIsPresent);
     _copyScreen->sdCardStateChange(cardIsPresent);
     _initiatorMainScreen->sdCardStateChange(cardIsPresent);
+    _noControlsErrorScreen->sdCardStateChange(cardIsPresent);
 }
 
 // Return a pointer to a screen
@@ -177,6 +183,9 @@ Screen *GetScreen(SCREEN_TYPE type)
 
         case SCREEN_INITIATOR_MAIN:
             return _initiatorMainScreen;
+
+        case SCREEN_NO_CONTROLS_ERROR:
+            return _noControlsErrorScreen;
     }
     return NULL;
 }
@@ -226,6 +235,10 @@ const char *GetScreenName(SCREEN_TYPE type)
 
         case SCREEN_INITIATOR_MAIN:
             return "Initiator Main";
+
+        case SCREEN_NO_CONTROLS_ERROR:
+            return "No Controls Error";
+
     }
     return "Unknown";
 }

commit 23e7c07aae1c7a0960e73b2f1ef870ad7b82a807

Fix crash when no control board is present
This fixes a crash when no control board is present. A regression
from https://github.com/ZuluSCSI/ZuluSCSI-firmware/pull/707

lib/ZuluSCSI_UI_RP2MCU/control.cpp
@@ -140,6 +140,8 @@ static struct
 int g_pcaAddr = 0x3F;
 
 bool g_controlBoardEnabled = false;
+bool g_displayEnabled = false;
+
 bool hasScreenBeenInitialized = false;
 bool hasControlBeenInitialized = false;
 
@@ -228,6 +230,9 @@ void printDevices()
  */
 static bool splashScreenPoll()
 {
+    if (!g_displayEnabled)
+        return false;
+
     if (g_uiStart != ZULUSCSI_UI_START_DONE)
     {
         if (g_uiStart == ZULUSCSI_UI_START_SPLASH_VERSION && (uint32_t)(millis() - g_uiSplashStartTime) > 1500)
@@ -455,6 +460,7 @@ bool initScreenHardware()
 
         return false;
     }
+    g_displayEnabled = true;
         // Clear the buffer
     g_display->clearDisplay();
     g_display->display();
@@ -752,6 +758,10 @@ void scanForNestedFolders()
 // Called on sd remove and Device Changes
 void stateChange()
 {
+    if (!g_displayEnabled)
+    {
+        return;
+    }
     g_userInputDetected = true;  // Something changed, so assume the user did interact
 
     sendSDCardStateChangedToScreens(g_sdAvailable);
@@ -1180,21 +1190,21 @@ extern "C" void binCueInUse(int target_idx, const char *foldername)
 }
 
 // When a card is removed or inserted. If it's removed then clear the device list
-extern "C" void sdCardStateChanged(bool sdAvailable)
+extern "C" void sdCardStateChanged(bool sdAvailable, bool romdrivePresent)
 {
 
     g_sdAvailable = sdAvailable;
 
-    if (!hasControlBeenInitialized) return;
-
     initDevices();
 
-    if (!sdAvailable) // blank the device map
+
+    if (!sdAvailable || romdrivePresent) // blank the device map
     {
         patchDevices();
         stateChange();
     }
 }
+
 
 // On startup on when a card has finished been reinserted, this is call. Used to call 2nd pass of device setup
 extern "C" void scsiReinitComplete()

lib/ZuluSCSI_audio_RP2MCU/audio_spdif.cpp
@@ -375,7 +375,7 @@ bool audio_is_playing(uint8_t id) {
 
 void audio_setup() {
 #ifdef CONTROL_BOARD
-    if (!g_scsi_settings.getSystem()->enableCDAudio || g_controlBoardEnabled)
+    if (!g_scsi_settings.getSystem()->enableCDAudio || g_controlBoardEnabled || g_displayEnabled)
 #else
     if (!g_scsi_settings.getSystem()->enableCDAudio)
 #endif

lib/ZuluSCSI_platform_GD32F205/ui.cpp
@@ -22,7 +22,7 @@
 #include "ui.h"
 
 extern "C" void scsiReinitComplete() {}
-extern "C" void sdCardStateChanged(bool sdAvailable) {}
+extern "C" void sdCardStateChanged(bool sdAvailable, bool romdrivePresent) {}
 extern "C" void controlLoop() {}
 extern "C" bool mscMode() { return false; }
 extern "C" void setRootFolder(int target_idx, bool userSet, const char *path) {}
@@ -32,6 +32,7 @@ extern "C" void initUIDisplay() {}
 extern "C" void initUIPostSDInit(bool cardPresent) {}
 
 bool g_controlBoardEnabled = false;
+bool g_displayEnabled = false;
 
 bool g_initiatorMessageToProcess;
 

lib/ZuluSCSI_platform_GD32F450/ui.cpp
@@ -31,6 +31,7 @@ extern "C" void initUIDisplay() {}
 extern "C" void initUIPostSDInit(bool cardPresent) {}
 
 bool g_controlBoardEnabled = false;
+bool g_displayEnabled = false;
 
 bool g_initiatorMessageToProcess;
 

lib/ZuluSCSI_platform_RP2MCU/ZuluSCSI_platform.cpp
@@ -1332,7 +1332,7 @@ void platform_reset_mcu(uint32_t reset_in_ms)
 uint8_t platform_get_buttons()
 {
     static bool init_buttons = false;
-    if (!g_controlBoardEnabled) // use legacy button pressing stuff
+    if (!g_displayEnabled) // use legacy button pressing stuff
     {
         uint8_t buttons = 0;
 
@@ -1341,7 +1341,7 @@ uint8_t platform_get_buttons()
         {   // pulled to VCC via resistor, sinking when pressed
             if (!gpio_get(GPIO_EXP_SPARE)) buttons |= 1;
         }
-        else if (!g_controlBoardEnabled)
+        else if (!g_displayEnabled)
         {
             if (!init_buttons)
             {

lib/ZuluSCSI_platform_RP2MCU/ui.cpp
@@ -23,9 +23,10 @@
 
 #ifndef CONTROL_BOARD
 bool g_controlBoardEnabled = false;
+bool g_displayEnabled = false;
 
 extern "C" void scsiReinitComplete() {}
-extern "C" void sdCardStateChanged(bool sdAvailable) {}
+extern "C" void sdCardStateChanged(bool sdAvailable, bool romdrivePresent) {}
 extern "C" void controlLoop() {}
 extern "C" bool mscMode() { return false; }
 extern "C" void setRootFolder(int target_idx, bool userSet, const char *path) {}

src/ZuluSCSI.cpp
@@ -1097,10 +1097,6 @@ static void zuluscsi_setup_sd_card(bool wait_for_card = true)
 {
   g_sdcard_present = mountSDCard();
 
-  bool orig_g_sdcard_present = g_sdcard_present;
-
-  sdCardStateChanged(g_sdcard_present);
-
   if(!g_sdcard_present)
   {
     if (SD.sdErrorCode() == platform_no_sd_card_on_init_error_code())
@@ -1128,6 +1124,10 @@ static void zuluscsi_setup_sd_card(bool wait_for_card = true)
       reinitSCSI();
       if (g_romdrive_active)
       {
+        if (g_displayEnabled)
+        {
+          sdCardStateChanged(g_sdcard_present, g_romdrive_active);
+        }
         logmsg("Enabled ROM drive without SD card");
         return;
       }
@@ -1137,20 +1137,18 @@ static void zuluscsi_setup_sd_card(bool wait_for_card = true)
     {
       blinkStatus(BLINK_ERROR_NO_SD_CARD);
 
-      uint32_t input_wait_start = millis();
-      while ((uint32_t)(millis() - input_wait_start) < 500)
+      if (g_displayEnabled)
       {
-        controlLoop();
+        uint32_t input_wait_start = millis();
+        while ((uint32_t)(millis() - input_wait_start) < 500)
+        {
+          controlLoop();
+        }
       }
 
       platform_reset_watchdog();
       g_sdcard_present = mountSDCard();
 
-      if (g_sdcard_present != orig_g_sdcard_present)
-      {
-          sdCardStateChanged(g_sdcard_present);
-      }
-  
     } while (!g_sdcard_present && wait_for_card && !g_rebooting);
     blink_cancel();
     LED_OFF();
@@ -1163,6 +1161,10 @@ static void zuluscsi_setup_sd_card(bool wait_for_card = true)
     {
       logmsg("Continuing without SD card");
     }
+  }
+  if (g_displayEnabled)
+  {
+    sdCardStateChanged(g_sdcard_present, g_romdrive_active);
   }
   check_for_unused_update_files();
   firmware_update();
@@ -1338,7 +1340,7 @@ extern "C" void zuluscsi_main_loop(void)
   blink_poll();
 
 
-  if (scsiDev.phase == BUS_FREE)
+  if (g_displayEnabled && scsiDev.phase == BUS_FREE)
   {
     controlLoop();
   }
@@ -1389,7 +1391,10 @@ extern "C" void zuluscsi_main_loop(void)
           g_sdcard_present = false;
           logmsg("SD card removed, trying to reinit");
 
-          sdCardStateChanged(false);
+          if (g_displayEnabled)
+          {
+            sdCardStateChanged(g_sdcard_present, g_romdrive_active);
+          }
         }
       }
     }
@@ -1410,26 +1415,27 @@ extern "C" void zuluscsi_main_loop(void)
         blink_cancel();
         LED_OFF();
         logmsg("SD card reinit succeeded");
-
-        sdCardStateChanged(true);
-
         print_sd_info();
         reinitSCSI();
         init_logfile();
         init_eject_button();
         blinkStatus(BLINK_STATUS_OK);
+        if (g_displayEnabled)
+        {
+          sdCardStateChanged(g_sdcard_present, g_romdrive_active);
+        }
       }
       else if (!g_romdrive_active)
       {
         blinkStatus(BLINK_ERROR_NO_SD_CARD);
         platform_reset_watchdog();
         platform_poll();
-        uint32_t input_wait_start = millis();
-        while ((uint32_t)(millis() - input_wait_start) < 500)
+        if (g_displayEnabled)
         {
-          if (scsiDev.phase == BUS_FREE)
+          uint32_t input_wait_start = millis();
+          while (scsiDev.phase == BUS_FREE && (uint32_t)(millis() - input_wait_start) < 500)
           {
-            controlLoop();
+              controlLoop();
           }
         }
       }

src/ZuluSCSI_config.h
@@ -27,8 +27,8 @@
 #include <ZuluSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2025.11.26"
-#define FW_VER_SUFFIX   "release"
+#define FW_VER_NUM      "2025.12.01"
+#define FW_VER_SUFFIX   "dev"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR

src/ui.h
@@ -45,7 +45,8 @@ typedef enum
 #define MAX_CATEGORY_NAME_LEN 32
 
 extern "C" void scsiReinitComplete();
-extern "C" void sdCardStateChanged(bool sdAvailable);
+extern "C" void sdCardStateChanged(bool sdAvailable, bool romdrive_present);
+
 
 extern "C" void controlLoop();
 extern "C" bool mscMode();
@@ -60,6 +61,7 @@ extern "C" void initUIPostSDInit(bool cardPresent);
 extern "C" void initScreens();
 
 extern bool g_controlBoardEnabled;
+extern bool g_displayEnabled;
 
 #if defined(CONTROL_BOARD)
 