7aff7447bf8af4c597844312aa4e577792bac633
Alex Perez <aperez@rabbithole.ws>
2025-12-18 12:25:04 -0800

    Prepare for v2025.12.18 release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -28,7 +28,7 @@
 
 // Use variables for version number
 #define FW_VER_NUM      "2025.12.18"
-#define FW_VER_SUFFIX   "dev"
+#define FW_VER_SUFFIX   "release"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
