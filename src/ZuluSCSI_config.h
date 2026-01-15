c2ecd5c0fbfe4a5b5f81d811ac013b3e24dccac6
Alex Perez <aperez@rabbithole.ws>
2026-01-15 14:47:07 -0800

    Prepare for Blaster-specific release

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,8 +27,8 @@
 #include <ZuluSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2025.12.19"
-#define FW_VER_SUFFIX   "dev"
+#define FW_VER_NUM      "2026.01.15"
+#define FW_VER_SUFFIX   "release"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
