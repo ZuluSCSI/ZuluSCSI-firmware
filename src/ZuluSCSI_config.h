2720d167344d7030f38740521e44988024eed125
J. Morio Sakaguchi <morio.earth@gmail.com>
2026-03-18 11:38:39 -0700

    Bump version to 2026.03.18 debug

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,8 +27,8 @@
 #include <ZuluSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2026.03.12"
-#define FW_VER_SUFFIX   "release"
+#define FW_VER_NUM      "2026.03.18"
+#define FW_VER_SUFFIX   "debug"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
 #define STRINGFY(STR) #STR
