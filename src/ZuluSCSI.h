ef5c7316f4810a7affd57356a1405967c29ab618
J. Morio Sakaguchi <morio.earth@gmail.com>
2026-04-06 14:54:18 -0700

    Add VHD support to CREATE image generator
    It is possible to create an image by making a zero byte filename
    like "CREATE 123M HD1 - my file.txt" to create a 123 MB image file
    named "CREATE 123M HD1 - my file.img".
    
    Now if the same file with the extension `.vhd` is made
    ("CREATE 123M HD1 - my file.vhd") the ZuluSCSI will create a VHD file
    called "HD1 - myfile.vhd".
    
    If for some reason the footer can not be added, the ZuluSCSI will
    attempt to rename the file to "HD1 - my file.raw" indicating the
    file does not have a VHD footer. The error will be logged in the
    `zululog.txt` file. This is done because large images can take a while
    to create and the image can still be converted to a VHD with other
    software.

--- src/ZuluSCSI.h
+++ src/ZuluSCSI.h
@@ -8,6 +8,7 @@ extern bool g_sdcard_present;
 extern SdFs SD;
 
 // Create an zero-filled image file of specified size -- update UI with progress
+//  If .vhd file footer fails to write renames extension to .raw
 //	Returns true if image created
 //	Returns false if file already exists or on error
-bool createImageFile(const char *imgname, uint64_t size);
+bool createImageFile(char *imgname, uint64_t size);
