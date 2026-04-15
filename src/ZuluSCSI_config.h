11939483afcdc8407e24233ccafce35bdc24f106
Alex Perez <aperez@rabbithole.ws>
2026-04-15 11:41:33 -0700

    iterate dev version string

--- src/ZuluSCSI_config.h
+++ src/ZuluSCSI_config.h
@@ -27,7 +27,7 @@
 #include <ZuluSCSI_platform_config.h>
 
 // Use variables for version number
-#define FW_VER_NUM      "2026.04.13"
+#define FW_VER_NUM      "2026.04.15"
 #define FW_VER_SUFFIX   "dev"
 
 #define DEF_STRINGFY(DEF) STRINGFY(DEF)
